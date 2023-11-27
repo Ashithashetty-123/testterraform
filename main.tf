06b971e289fd44e8d3e324a86f5f2bfaed3264ce
Ashithashetty-123 <113819589+Ashithashetty-123@users.noreply.github.com>
2023-11-27 11:06:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   access_key = "AKIAW64YYHOFTWKBAIT5"
   secret_key = "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"
 }
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "myproject-2023-11-07-4e35c7d0-0742-11ec-9a03-0242ac1300032000" {
   bucket = "myproject-2023-11-07-4e35c7d0-0742-11ec-9a03-0242ac1300032000"
   acl    = "public  "
 }
