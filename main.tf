8f43c9258979559eff2f43e791f720205ef0935e
Ashithashetty-123 <113819589+Ashithashetty-123@users.noreply.github.com>
2023-12-23 16:11:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "aws" {
   secret_key = "Wirw5mwFpWKiH+IWibDhG4jmRJJSOREeEc0D91ir"
 }
 resource "aws_s3_bucket" "myproject-2023-11-07-4e35c7d0-0742-11ec-9a03-0242ac1300032000" {
-  bucket = "myproject-2023-11-07-4e35c7d0-0742-11ec-9a03-0242ac1300032000"
+  bucket = "myproject-2023-11-07-4e35c7d0-0742-11ec-9a03-0242ac1300032000111"
   
 }
